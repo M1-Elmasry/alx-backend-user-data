759390cf1e40b705c501224b27e7093bc1d869e0
M1-Elmasry <mostafamfelmasry@outlook.com>
2024-07-12 06:58:44 +0300

    add forbidden handler

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -32,3 +32,11 @@ def unauthorized() -> None:
     test unauthorized request
     """
     abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> None:
+    """ GET /api/v1/forbidden
+    test forbidden request
+    """
+    abort(403)
