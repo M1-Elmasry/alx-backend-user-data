43de368444cf1f1d774224a1a919b13d7f11b261
M1-Elmasry <mostafamfelmasry@outlook.com>
2024-07-13 04:51:41 +0300

    add Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" 
+    module for authentication operations
+"""
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """ Auth class
+    """
+
+    def require_auth(self, path: str, excluded_path: List[str]) -> bool:
+        """
+        will implement later
+        """
+        return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """
+        will implement later
+        """
+        return None
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        will implement later
+        """
+        return None
